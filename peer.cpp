7bd01813d2dba23b6e35f8d5a7aeff555123cc37
Divyansh Shrivastava <divyansh.shrivastava@students.iiit.ac.in>
2019-10-11 19:05:51 +0530

    made many changes

--- peer.cpp
+++ peer.cpp
@@ -5,9 +5,24 @@
 #include <unistd.h>
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <pthread.h>
 #include <arpa/inet.h>
 
+struct transfer_unit
+{
+	const char* my_IP ;
+	const int my_port ;
+};
+
+bool logged_in ;
+char *my_IP ;
+int my_port ;
+char *tracker_info_file ;
+
 int read_tracker_info(char*, int&, const char*);
+void* listen_as_server(void*) ;
+void login_and_register(const char*, const char*);
+void create_user(const char*, const char*) ;
 
 int main(int argc, char* argv[])
 {
@@ -17,17 +32,81 @@ int main(int argc, char* argv[])
 		return 0 ;
 	}
 
-	const char* my_IP = argv[1] ;
-	const int my_port = atoi(argv[2]) ;
-	const char* tracker_info_file = argv[3] ;
+	logged_in = false ;
+	my_IP = argv[1] ;
+	my_port = atoi(argv[2]) ;
+	tracker_info_file = argv[3] ;
+
+	char command[1024] ;
+	while(1)
+	{
+		printf(">> ");
+		fgets(command, sizeof command, stdin) ;
+		char *cmd = strtok((char*)command, " \n");
+		if(strcmp(cmd, "login") == 0)
+		{
+			char *uname = strtok(NULL, " \n") ;
+			char *pwd = strtok(NULL, " \n") ;
+			login_and_register(uname, pwd) ;
+			if(!logged_in) continue ;	
+		} else if (strcmp(cmd, "create_user") == 0)
+		{
+			char *uname = strtok(NULL, " \n") ;
+			char *pwd = strtok(NULL, " \n") ;
+			create_user(uname, pwd) ;
+		} else printf("Undefined command.\n") ;
+	}
+	
+	return 0 ;
+}
+
+void login_and_register(const char* username, const char* passwd)
+{
+	if(logged_in) return ;
+	FILE *fp ;
+	fp = fopen("authentication.txt", "r");
+	if(fp == 0)
+	{
+		printf("login failed as no file found.\n");
+		return ;	
+	}
+	char uname[32] ;
+	char pwd[32] ;
+	bool valid = false ;
+	while(fscanf(fp, "%s%s", uname, pwd) != EOF)
+	{
+		if(strcmp(uname, username) == 0)
+		{
+			if(strcmp(passwd, pwd) == 0)
+			{
+				valid = true ;
+				break ;
+			} else printf("Invalid Password.\n") ;
+		}
+	}
+	fclose(fp) ;
+	if(!valid)
+	{
+		printf("Login failed.\n") ;
+		return ;
+	}
+
+	logged_in = true ;
+
+	struct transfer_unit tu = {my_IP, my_port};
+
+	pthread_t listen_thread ;
+	pthread_create(&listen_thread, NULL, listen_as_server, (void*)&tu);
+	printf("Listening thread detached.\n");
 
 	char tracker_IP[32] ;
 	int tracker_port ;
 
 	if(read_tracker_info(tracker_IP, tracker_port, tracker_info_file) < 0)
 	{
+		/* ADD LOGIC FOR TWO TRACKER SYSTEM */
 		printf("Tracker info file seems to be unreadable/empty.\n") ;
-		return 0 ;
+		return ;
 	}
 
 	printf("I read %s and %d\n", tracker_IP, tracker_port) ;
@@ -36,22 +115,26 @@ int main(int argc, char* argv[])
 	if( (tracker_socket = socket(AF_INET, SOCK_STREAM, 0)) < 0)
 	{
 		printf("Cannot create the socket.\n") ;
-		return 0 ;
+		return;
 	}
 
 	struct sockaddr_in tracker_addr ;
 
 	bzero( (char*)&tracker_addr, sizeof tracker_addr ) ;
 	tracker_addr.sin_family = AF_INET ;
-	tracker_addr.sin_port = tracker_port ;
+	tracker_addr.sin_port = htons(tracker_port) ;
 	tracker_addr.sin_addr.s_addr = inet_addr(tracker_IP) ;
 
 	if( connect(tracker_socket, (struct sockaddr*)&tracker_addr, sizeof tracker_addr ) < 0 )
 	{
 		printf("cannot connect to the tracker.\n") ;
 		close(tracker_socket) ;
-		return 0 ;
+		return ;
 	}
+
+	char data[128] ;
+	sprintf(data, "%s %d", my_IP, my_port);
+	send(tracker_socket, data, sizeof data, 0) ;
 
 	char msg[100] ;
 	recv(tracker_socket, msg, sizeof msg, 0) ;
@@ -59,7 +142,19 @@ int main(int argc, char* argv[])
 	printf("Tracker sent me : %s\n", msg) ;
 
 	close(tracker_socket) ;
-	return 0 ;
+}
+
+void create_user(const char* uname, const char* pwd)
+{
+	if(uname == NULL || pwd == NULL) return ;
+	FILE *fp = fopen("authentication.txt", "a") ;
+	if(fp == 0) 
+	{
+		printf("cannot create a user.\n") ;
+		return ;
+	}
+	fprintf(fp, "%s %s", uname, pwd);
+	fclose(fp) ;
 }
 
 int read_tracker_info(char tracker_IP[32], int &tracker_port, const char* tracker_info_file)
@@ -72,4 +167,42 @@ int read_tracker_info(char tracker_IP[32], int &tracker_port, const char* tracke
 	}
 	fclose(tracker) ;
 	return 1 ;
+}
+
+void* listen_as_server(void* args)
+{
+
+	struct transfer_unit *tu = (struct transfer_unit*)args ;
+
+	int listen_socket ;
+	if( (listen_socket = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+	{
+		printf("Couldn't create the listening socket.\n") ;
+		return NULL ;
+	}
+
+	struct sockaddr_in list_sock ;
+	bzero( (char*)&list_sock, sizeof list_sock );
+	list_sock.sin_family = AF_INET ;
+	list_sock.sin_port = htons(tu->my_port) ;
+	list_sock.sin_addr.s_addr = inet_addr(tu->my_IP) ;
+
+	if(bind(listen_socket, (struct sockaddr*)&list_sock, sizeof list_sock) < 0)
+	{
+		printf("bind failed.\n");
+		close(listen_socket) ;
+		return NULL ;
+	}
+
+	listen(listen_socket, 10) ;
+
+	int sz = sizeof list_sock ;
+	int active_sock = accept(listen_socket, (struct sockaddr*)&list_sock, (socklen_t*)&sz) ;
+
+	char msg[100] = "hahaha this client is a server as well lol.\n" ;
+	send(active_sock, msg, sizeof msg, 0) ;
+
+	close(active_sock);
+	close(listen_socket) ;
+	pthread_exit(NULL) ;
 }
--- tracker.cpp
+++ tracker.cpp
@@ -1,14 +1,100 @@
 #include <cstdio>
 #include <cstring>
 #include <cstdlib>
+#include <string>
+#include <vector>
+#include <iostream>
 #include <netinet/in.h>
 #include <unistd.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <arpa/inet.h>
+#include <pthread.h>
 
 #define TRACKER_IP "127.0.0.1"
 #define TRACKER_PORT 33333
+
+std::vector<std::string> client_info_vector ;
+
+struct transfer_unit
+{
+	int peer_socket ;
+	struct sockaddr_in c_addr ;
+};
+
+void update_client_info_file()
+{
+	FILE *fp = fopen("client_info.txt", "w");
+	if(fp == 0)
+	{
+		printf("The Client info file cannot be opened.\n") ;
+	} else
+	{
+		for(const std::string &s : client_info_vector)
+		{
+			const char* tmp = s.c_str() ;
+			fprintf(fp, "%s\n", tmp);
+		}
+	}
+	fclose(fp) ;
+}
+
+int connect_peer(const char* IP, const int port)
+{
+	int active_sock ;
+	if( (active_sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+	{
+		printf("Couldn't create the active socket.\n") ;
+		return -1;
+	}
+
+	struct sockaddr_in act_sock ;
+	bzero( (char*)&act_sock, sizeof act_sock );
+	act_sock.sin_family = AF_INET ;
+	act_sock.sin_port = htons(port) ;
+	act_sock.sin_addr.s_addr = inet_addr(IP) ;
+
+	//TODO now
+	if(connect(active_sock, (struct sockaddr*)&act_sock, sizeof act_sock) < 0)
+	{
+		printf("cannot connect.\n") ;
+		return -1; 
+	}
+
+	char msg[100] ;
+	recv(active_sock, msg, sizeof msg, 0) ;
+	printf("The server side of the client sent me : %s\n", msg) ;
+
+	close(active_sock); 
+	return 0 ;
+}
+
+void* communicate_peer(void* args)
+{
+	struct transfer_unit *tf = (transfer_unit*)args ;
+
+	char data[128] ;
+	recv(tf->peer_socket, data, sizeof data, 0);
+	char IP[32] ;
+	int port ;
+	sscanf(data, "%s%d", IP, &port);
+
+	printf("I got : %s and %d\n", IP, port) ;
+
+	if(connect_peer(IP, port) == 0)
+	{
+		std::string temp(data) ;
+		client_info_vector.push_back(temp);
+		update_client_info_file();
+	}
+
+	char buff[32] = "Hello from the Tracker!\n" ;
+	send(tf->peer_socket, buff, sizeof buff, 0) ;
+
+	close(tf->peer_socket) ;
+	printf("Client seen : %s : %d\n", inet_ntoa(tf->c_addr.sin_addr), tf->c_addr.sin_port);
+	pthread_exit(NULL) ;
+}
 
 int main(int argc, char* argv[])
 {
@@ -32,7 +118,7 @@ int main(int argc, char* argv[])
 
 	bzero( (char*)&s_addr, sizeof s_addr );
 	s_addr.sin_family = AF_INET;
-	s_addr.sin_port = TRACKER_PORT ;
+	s_addr.sin_port = htons(TRACKER_PORT) ;
 	s_addr.sin_addr.s_addr = inet_addr(TRACKER_IP) ;
 
 	if(bind(tracker_socket, (struct sockaddr*) &s_addr, sizeof s_addr) < 0)
@@ -45,6 +131,7 @@ int main(int argc, char* argv[])
 	listen(tracker_socket, 10) ;
 	unsigned int client_sock_size = sizeof c_addr ;
 
+	/*
 	peer_socket = accept(tracker_socket, (struct sockaddr*)&c_addr, &client_sock_size);
 	if(peer_socket < 0)
 	{
@@ -57,6 +144,15 @@ int main(int argc, char* argv[])
 	send(peer_socket, buff, sizeof buff, 0) ;
 
 	close(peer_socket) ;
+	*/
+	while(1)
+	{
+		peer_socket = accept(tracker_socket, (struct sockaddr*)&c_addr, &client_sock_size);
+		transfer_unit tf = {peer_socket, c_addr} ;
+		pthread_t tid ;
+		pthread_create(&tid, NULL, communicate_peer, (void*)&tf);
+	}
+
 	close(tracker_socket) ;
 
 	return 0 ;
